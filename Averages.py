eb33e31a2c17b4eae8eca7691811815a4645b8ed
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-11 14:24:54 -0400

    Deleted the non working code.

--- Averages.py
+++ Averages.py
@@ -1,7 +1,6 @@
 """Average weight program"""
 import psycopg2
 import itertools
-import time
 
 #weight_id, weight, date_created
 # Enter a, q  or a number to the database.
@@ -14,8 +13,7 @@ try:
     CUR = CONN.cursor()
 except psycopg2.OperationalError as e:
     print("No database by that name exists.")
-time.time = float(time.time())
-run_time  = time.time()
+
 
 
 def main():
@@ -140,7 +138,6 @@ def helper():
 if  __name__ == "__main__":
     try:
         main()
-        print("*** %s seconds to complete ****" % (float(time.time) - run_time))
     except NameError as e:
         print("No variable by that name exists."+ "\n"+ str(e))
   
